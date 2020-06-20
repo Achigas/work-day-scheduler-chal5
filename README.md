4e4c1ffd11590bf10fe1afc90010bdc62025ea9d
AChigas <Achigas34@gmail.com>
2020-06-20 01:07:36 -0700

    for loop for array

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Work Day Scheduler Starter Code
+A planner for your everyday business needs!
+
+## Table of contents
+* [General info](#general-info)
+* [Screenshots](#screenshots)
+* [URL](#url)
+* [Code](#code
+* [Status](#status)
+* [Inspiration](#inspiration)
+* [Contact](#contact)
+
+## General info
+The purpose of the project is to create a daily planner that helps to schedule your work day. 
+
+## Screenshots
+![Example screenshot](./img/screenshot.png)
+
+## URL
+[]()
+
+## Code
+Show examples of usage:
+`put-your-code-here`
+
+## Status
+Project is complete.
+
+## Inspiration
+Alex Chigas. Project inspired by UCLAx Challenge 5.
+
+## Contact
+Created by Alex Chigas
---
+++ assets/js/script.js
@@ -0,0 +1,123 @@
+//initialize variable
+var dayPlanner = []
+
+//for loop to create array for loop
+for (time = 9; time <= 17; time++) {
+    //get index and set variable to store data in array
+    var id = time - 9
+    var dataPlanner = ""
+   
+
+   //Get display time 
+   var displayHour = 0;
+   var ampm = "";
+
+
+    if (time === 12) {
+        displayHour = 12
+        ampm = "pm"
+    } else if (time > 12) { 
+       displayHour = time - 12;
+       ampm = "pm";
+   } else if (time < 12) {
+       displayHour = time;
+       ampm = "am";
+   }
+
+   displayHour = displayHour.toString()
+
+   dataPlanner = {
+       id: id,
+       displayHour: displayHour,
+       time: time,
+       ampm: ampm,
+       dataPlanner: dataPlanner
+   }
+
+   dayPlanner.push(dataPlanner)
+
+}
+
+//end array built
+
+//current day for header 
+function getCurrentDate() {
+    var currentDate = moment().format('dddd, MMMM Do');
+    $("#currentDay").text(currentDate);
+}
+
+//saves data to localStorage
+function savePlannerData() {
+    localStorage.setItem("dayPlanner", JSON.stringify(dayPlanner));
+}
+
+//displays data in time slots
+function displayPlannerData() {
+    dayPlanner.forEach(function (hour) {
+        $($(hour.id).val(hour.dataPlanner))
+    }) 
+}
+
+//load data function -- runs save and display
+function loadPlannerData() {
+    var dataLoaded = JSON.parse(localStorage.getItem("dayPlanner"));
+
+    if (dataLoaded) {
+        dayPlanner = dataLoaded;
+    }
+
+    savePlannerData()
+    displayPlannerData()
+}
+
+dayPlanner.forEach(function(hour) {
+    // creates row
+    var timeRow = $("<form>")
+        .addClass("row");
+
+    $(".container").append(timeRow);
+
+    //creates field for time
+    var timeField = $("<div>")
+        .addClass("col-md-2 hour")
+        .text(hour.displayHour + hour.ampm);
+
+    // creates schdeduler data
+    var hourInput = $("<div>")
+        .addClass("col-md-9 description p-0")
+    var hourData = $("<textarea>");
+        hourData.attr("id", hour.id);
+
+        if (hour.time < moment().format("HH")) {
+            hourData.addClass("past")
+        } else if (hour.time === moment().format("HH")) {
+            hourData.addClass("present")
+        } else if (hour.time > moment().format("HH")) {
+            hourData.addClass("future")
+    }
+
+    hourInput.append(hourData);
+    
+    // creates save button
+    var saveButton = $("<i class='far fa-save fa-lg'></i>")
+    var savePlan = $("<button>")
+        .addClass("col-md-1 saveBtn");
+
+    savePlan.append(saveButton);    
+    timeRow.append(timeField, hourInput, savePlan)
+})
+
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    var saveIndex = $(this).siblings(".description").children(".future").attr("id");
+    dayPlanner[saveIndex].dataPlanner = $(this).siblings(".description").children(".future").val();
+    console.log(saveIndex);
+    savePlannerData();
+    displayPlannerData();
+})
+
+
+//get current date on page load
+getCurrentDate()
+//load data for page load
+loadPlannerData()
